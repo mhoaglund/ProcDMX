d1a06d1a2a5c654de53320cc01123a66d85e46de
mhoaglund <maxwell.hoaglund@gmail.com>
2016-12-31 15:27:55 -0600

    finished laborious array grooming method

--- synchronousplayer.py
+++ synchronousplayer.py
@@ -248,36 +248,67 @@ class SyncPlayer(Process):
         time.sleep(self.delay)
 
     def groom(self, _list):
-        """Given an array of readings, fill in obvious gaps and spot edges"""
-        listsz = len(_list)-1
+        """Given an array of readings, fill in obvious gaps, spot edges, spot contiguous regions"""
+        listsz = len(_list)
         islands = []
+        pits = []
+        holes = []
         edges = []
         contigs = []
         curr = []
-        #find contiguous regions of positive values and log them by index
-        #TODO filling in single zeros
+        currpits = []
         for value in range(0, listsz):
-            if _list[value] == 0 and len(curr) == 0:
-                continue
-            elif _list[value] == 0 and len(curr) > 0:
-                if len(curr) > 1:
-                    contigs.append(curr)
-                    edgepair = []
-                    if curr[0] > 0:
-                        edgepair.append(curr[0]-1)
-                    if curr[len(curr)-1] < listsz:
-                        edgepair.append(curr[len(curr)-1]+1)
-                    if len(edgepair) > 1:
-                        edges.append(edgepair)
-                else:
-                    islands.append(curr)
-                curr = []
-            elif _list[value] > 0 and len(curr) > 0:
-                curr.append(value)
-            elif _list[value] > 0 and len(curr) == 0:
-                #start a new contiguous region
-                curr = [] #superstition
-                curr.append(value)
+            if _list[value] == 0:
+                if len(currpits) > 0:
+                    currpits.append(value)
+                if len(currpits) == 0:
+                    currpits = []
+                    currpits.append(value)
+                if len(curr) == 0:
+                    continue
+                if len(curr) > 0:
+                    if len(curr) > 1:
+                        contigs.append(curr)
+                        edgepair = []
+                        if curr[0] > 0:
+                            edgepair.append(curr[0]-1)
+                        if curr[len(curr)-1] < listsz:
+                            edgepair.append(curr[len(curr)-1]+1)
+                        if len(edgepair) > 1:
+                            edges.append(edgepair)
+                    else:
+                        islands.append(curr)
+                    curr = []
+
+            if _list[value] > 0:
+                if len(curr) > 0:
+                    curr.append(value)
+                if len(curr) == 0:
+                    curr.append(value)
+
+                if len(currpits) > 0:
+                    if len(currpits) > 1:
+                        pits.append(currpits)
+                    else:
+                        holes.append(currpits)
+                    currpits = []
+
+        if len(currpits) > 0:
+            if len(currpits) > 1:
+                pits.append(currpits)
+            else:
+                holes.append(currpits)
+            currpits = []
+
+        if len(curr) > 0:
+            if len(curr) > 1:
+                contigs.append(curr)
+            else:
+                islands.append(curr)
+            curr = []
+
+        for hole in holes:
+            _list[hole[0]] = 1
 
     def recchannel(self, old, mod, i, _mode):
         """Reconcile channel value with modifier, soft-clamping values modally """
