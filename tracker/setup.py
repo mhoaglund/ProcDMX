796e980d7eb144e0b3e9e7c0967bd7491de10342
Maxwell Hoaglund <maxwell.hoaglund@gmail.com>
2017-04-18 13:04:37 -0500

    adding distutils setup

---
+++ tracker/setup.py
@@ -0,0 +1,17 @@
+!#/usr/bin/env python
+
+"""
+Setup.py for multitrackstereo module
+"""
+
+from distutils.core import setup, Extension
+
+multitrackstereo_module = Extension('_multitrackstereo', sources=['multitrackstereo_wrap.cpp','multitrackstereo.cpp'])
+
+setup (
+    name = 'multitrackstereo',
+    author = 'mhoaglund',
+    description = 'stereo vision handler with gpu acceleration',
+    ext_modules = [multitrackstereo_module],
+    py_modules = ['multitrackstereo']
+    )
