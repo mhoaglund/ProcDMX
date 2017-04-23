9add24ef0dc48852f585f32118925791366dc39a
mhoaglund <maxwell.hoaglund@gmail.com>
2017-04-23 15:41:43 -0500

    tweaks from device

--- CVstream.py
+++ CVstream.py
@@ -112,7 +112,7 @@ class CVStream(Process):
                 IS_SHAPE_SET = True
             self.my_contour_queue.put(current_contours)
             cv2.imshow(str(self.stream_id), gray)
-            cv2.waitKey(15)
+            cv2.waitKey(10)
 
     def GenerateMask(self, _frame):
         """
