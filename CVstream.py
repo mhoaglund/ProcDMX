eb7a5d5d6260fa5d9103e3901fe9e074dc2defc2
Maxwell Hoaglund <maxwell.hoaglund@gmail.com>
2017-04-24 12:31:05 -0500

    trying this again just for fun

--- CVstream.py
+++ CVstream.py
@@ -95,6 +95,7 @@ class CVStream(Process):
             toshow = thresh.copy()
             for c in cnts:
                 (x, y, w, h) = cv2.boundingRect(c)
+                _rect = cv2.minAreaRect(c)
                 if cv2.contourArea(c) > self.settings.detectionMinimum and w > 2:
                     cdc = playerutils.CalcdContour(x, y, w, h, self.stream_id)
                     cdc.area = cv2.contourArea(c)
