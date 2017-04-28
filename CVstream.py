eea9e49483a723eee5472c4cbd0c5e0c7dbf0681
Maxwell Hoaglund <maxwell.hoaglund@gmail.com>
2017-04-28 18:13:01 -0500

    manually entering location matrix

--- CVstream.py
+++ CVstream.py
@@ -180,7 +180,12 @@ class CVStream(Process):
         print _res
         print "Sum:", sum(_res) #This sum shouldn't exceed STREAM_WIDTH
         _running = 0
-        for m in range(0, 136/2):
+        #doing this manually after all
+        if self.stream_id == "River":
+            _res=[23,23,23,21,21,20,20,19,19,18,18,17,17,17,16,16,15,15,14,14,13,13,13,12,12,12,11,11,10,10,10,9,9,9,8,8,8,7,7,7,7,6,6,6,6,5,5,5,5,4,4,4,4,3,3,3,3,3,3,2,2,2,2,2,2,2,1,1]
+        if self.stream_id == "City":
+            _res=[27,26,25,24,23,22,21,20,20,19,19,18,18,18,17,17,16,16,15,14,14,13,13,12,12,11,11,10,10,9,9,9,8,8,7,7,7,6,6,6,5,5,5,4,4,4,4,3,3,3,3,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1]
+        for m in range(0, 68):
             _start = _running
             _end = _running+_res[m]
             _running += _res[m]
