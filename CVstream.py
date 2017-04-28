ccefa5b6e2117eb814e70afe736d9ab09e8a8a92
Maxwell Hoaglund <maxwell.hoaglund@gmail.com>
2017-04-28 15:55:49 -0500

    visual tweaks for testing

--- CVstream.py
+++ CVstream.py
@@ -124,7 +124,7 @@ class CVStream(Process):
                             cv2.putText(thresh, str(cdc.spatialindex),(x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
             if self.shouldShow:
                 for point in self.settings.waypoints:
-                    cv2.rectangle(thresh, (point[0], 150), (point[0]+4, 150+4), (255, 255, 255), 1)
+                    cv2.rectangle(thresh, (point[0], point[1]), (point[0]+4, point[1]+4), (255, 255, 255), 1)
                     cv2.putText(thresh, str(point[2]),(point[0], point[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
             if len(current_contours) > 80: #camera must be changing exposure
                 current_contours = []
