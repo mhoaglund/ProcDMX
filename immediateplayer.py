f74c347e8e021b168348776634d02ab0ee11925a
Maxwell Hoaglund <maxwell.hoaglund@gmail.com>
2017-07-16 17:18:20 -0500

    hmmm

--- immediateplayer.py
+++ immediateplayer.py
@@ -237,7 +237,7 @@ class ImmediatePlayer(Process):
                 print "dying blank area..."
                 _color = self.colors.activations[randint(0, (len(self.colors.activations)-1))]
                 self.dye_memory(x, _color)
-            if _status[x] > 1:
+            else if _status[x] > 1:
                 #Pull color from color memory and dye it back.
                 _color = self.color_memory[x]
                 self.dye_memory(x, _color)
